98ee1098ca022290e1e511562f4b64b0ae7dfdb0
Zim <Zim@10.68.213.124>
2016-12-07 18:09:04 +0100

    Loaded in files.

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var BASE_URL = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
 var Settings = function (url) {
-	this.url = BASE_URL + url;
-	this.method = "GET";
-	this.qs = {format: 'json'};
-	this.headers = {
-		authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
-	};
+    this.url = BASE_URL + url;
+    this.method = "GET";
+    this.qs = {format: 'json'};
+    this.headers = {
+        authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
+    };
 };
 
 var Drone = function (id, name, mac) {
-	this._id = id;
-	this.name = name;
-	this.mac = mac;
+    this._id = id;
+    this.name = name;
+    this.mac = mac;
 };
 
 var File = function (id, first_record, last_record) {
@@ -33,16 +33,21 @@ var dronesSettings = new Settings("/drones?format=json");
 dal.clearDrone();
 
 request(dronesSettings, function (error, response, dronesString) {
-	var drones = JSON.parse(dronesString);
-	console.log(drones);
-	console.log("***************************************************************************");
-	drones.forEach(function (drone) {
-		var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
-		request(droneSettings, function (error, response, droneString) {
-			var drone = JSON.parse(droneString);
-			dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
-		});
-	});
+    var drones = JSON.parse(dronesString);
+    console.log(drones);
+    console.log("***************************************************************************");
+    drones.forEach(function (drone) {
+        var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
+        request(droneSettings, function (error, response, droneString) {
+            var drone = JSON.parse(droneString);
+            dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
+        });
+        var filesSettings = new Settings("/files?drone_id.is=" + drone.id + "&format=json");
+        request(filesSettings, function (error, response, filesString) {
+            console.log(filesString);
+        });
+    });
 });
+
 
 console.log("Hello World!");
